8eda3dfc6c4c0fc163cfcb6304fbbe443412e3a2
Wathisa <wathisa@gmail.com>
2026-04-24 16:41:37 +0700

    สร้างฟังก์ชันสร้างแผนที่และสุ่มตำแหน่งหลุมกับหมวก

--- main.js
+++ main.js
@@ -6,6 +6,34 @@ const fieldCharacter = "🌱";
 const playerCharacter = "🤓";
 
 class Field {
+  static generateField(rows, cols, holePercent) {
+    const field = [];
+
+    for (let i = 0; i < rows; i++) {
+      const row = [];
+
+      for (let j = 0; j < cols; j++) {
+        if (Math.random() < holePercent) {
+          row.push(hole);
+        } else {
+          row.push(fieldCharacter);
+        }
+      }
+
+      field.push(row);
+    }
+
+    //วางหมวกแบบสุ่ม
+    let hatRow = Math.floor(Math.random() * rows);
+    let hatCol = Math.floor(Math.random() * cols);
+    field[hatRow][hatCol] = hat;
+
+    //บังคับให้จุด 0,0 ไม่เป็นหลุม
+    field[0][0] = fieldCharacter;
+
+    return field;
+  }
+
   constructor(field) {
     this.field = field;
     this.playerRow = 0;
@@ -89,10 +117,5 @@ class Field {
   }
 }
 
-const myField = new Field([
-  ["🌱", "🌱", "🌱"],
-  ["🌱", "🕳", "🌱"],
-  ["🌱", "🎩", "🌱"],
-]);
-
+const myField = new Field(Field.generateField(10, 10, 0.1));
 myField.runGame();
